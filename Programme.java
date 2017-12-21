34089314374dcec566367d7764989645ea72812c
binouz <gabin.hiairrassary@hotmail.fr>
2017-12-21 22:31:14 +0100

    envoie cards

--- Programme.java
+++ Programme.java
@@ -8,7 +8,14 @@ public class Programme {
 
 	public static void main(String arg[]) {
 
+		Elf e = new Elf(1, "apero", 100, 10, 100, 10);
+		e.display();
 
+		Warrior w = new Warrior(2, "billy", 100, 50, 100, 100);
+		w.display();
+
+		w.attack(e);
+		e.display();
 
 	}
 }
