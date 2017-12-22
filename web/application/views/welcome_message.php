15d4d89aa7d751e6a767141ba66cd4e9a7bcd031
clementcros <crosclement34500@gmail.com>
2017-12-22 11:14:30 +0100

    Home page css + bootstrap

--- web/application/views/welcome_message.php
+++ web/application/views/welcome_message.php
@@ -3,30 +3,75 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to JDR</title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <body>
 
-<h1>WORK IN PROGRESS ;)
-	
-</h1>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">JDR JAVA</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Accueil</a></li>
+      <li><a href="#">règles</a></li>
+      <li><a href="#">apero</a></li>
+      <li><a href="#">remerciments</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Bienvenue sur le jdr</h3>
+  <p>Jdr est un projet développer en JAVA , PHP et react JS qui ce présente sous forme d'un mini jeux de combat tour par tour avec des cartes</p>
+</div>
+<br>
+<br>
+<div class="row">
+  <div class="col-sm-4">
+  <img src="http://static.mnium.org/images/contenu/actus/HearthStone/Cartes/funcards_chanteurs/David-Guetta.png">
+  </div>
+  <div class="col-sm-4">
+  <H3>Le concept :</h3>
+		<p>vous vous verez attribué 5 cartes aléatoirement avec des attaques, defence, pa diférentes</p>
+		<table>
+   <tr>
+       <td>Attaque</td>
+       <td>Defence</td>
+       <td>PA</td>
+   </tr>
+   <tr>
+       <td>150</td>
+       <td>500</td>
+       <td>3</td>
+   </tr>
+</table>
+  </div>
+  <div class="col-sm-4"></div>
+</div>
 
-<?php
-echo "ps une bière pour un developper pauvre svp";
-?>
+
 </body>
 
-
 <style>
-	h1 {
-		text-align : center;
-		color : red;
-	}
-	body {
-		background-image : url('https://zellox.com/wp-content/uploads/2016/05/background-website-%E2%80%AB403608205%E2%80%AC-%E2%80%AB%E2%80%AC.jpg');
-		background-size : cover;
-		background-repeat : no-repeat; 
-	}
-
+table
+{
+    border-collapse: collapse; /* Les bordures du tableau seront collées (plus joli) */
+	text-align : center;
+}
+td
+{
+    border: 1px solid black;
+}
+img {
+	height: 250;
+    width: 250px;
+	padding-left : 50px;
+}
 </style>
 </html>
